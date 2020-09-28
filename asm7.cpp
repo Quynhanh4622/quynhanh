9c66617d40127a967c05e216846aa1dd7aa49351
Quynhanh4622 <71160175+Quynhanh4622@users.noreply.github.com>
2020-09-28 21:20:37 +0700

    asm7

---
+++ asm7.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+	int n,i;
+	printf("n=");
+	scanf("%d",&n);
+	int arr[n];
+	int x;
+	printf("nhap so can tim kiem : ");
+	scanf("%d",&x);
+	
+	for(int i=0;i<n;i++){
+		printf("nhap cac gia tri : ");
+		scanf("%d",&arr[i]);
+		
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-1;j++){
+			if(arr[j]>arr[j+1]){
+				int tmp = arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=tmp;
+			}
+		}
+	}
+	for(int i=n-1;i>=0;i--){
+		if(arr[i]<x){
+			
+			printf("da tim thay :%d",arr[i]);
+			break;
+		}
+	}
+}
